9a7b1bfda619651d2b797cba4f58ad27a8ae2089
cjjujubee <connie.jew@gmail.com>
2016-07-07 13:07:59 -0700

    Finishes layout and setting up routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,21 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Do What You Gotta Do - A Todo App"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Do What You Gotta Do - A Todo App"
   end
 
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Do What You Gotta Do - A Todo App"
+  end
 end
