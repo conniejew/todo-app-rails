89ff87b3ddd28aa1c16949b53db4e091e2ba12fb
cjjujubee <connie.jew@gmail.com>
2016-07-08 00:12:18 -0700

    Adds ability to add and delete tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707205045) do
+ActiveRecord::Schema.define(version: 20160708063117) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "taskText"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
